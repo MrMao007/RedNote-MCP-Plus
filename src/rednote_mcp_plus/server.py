bfda9d722c856f3a8c7421d5f265852ab2ecded4
消闲 <maotianyang.mty@alibaba-inc.com>
2026-01-14 17:37:45 +0800

    login & publish

--- src/rednote_mcp_plus/server.py
+++ src/rednote_mcp_plus/server.py
@@ -1,6 +1,8 @@
 from mcp.server.fastmcp import FastMCP
 import os
 
+from rednote_mcp_plus.write import publish
+
 mcp = FastMCP()
 
 @mcp.tool()
@@ -9,4 +11,10 @@ def hello_world():
     return "hello world"
 
 if __name__ == "__main__":
-    mcp.run(transport='stdio') 
+    # mcp.run(transport='stdio') 
+    publish.publishText(
+        image_urls=["src/rednote_mcp_plus/static/images/ball.png"],
+        title="这是一个测试标题",
+        content="这是一个测试内容",
+        tags=["测试", "小红书"]
+    )
