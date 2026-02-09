351d4a7e3b6aca9208578249921484dcf6270753
消闲 <maotianyang.mty@alibaba-inc.com>
2026-02-09 16:45:14 +0800

    init

--- src/rednote_mcp_plus/write/publish.py
+++ src/rednote_mcp_plus/write/publish.py
@@ -28,7 +28,7 @@ async def publishText(image_urls: List[str], title: str, content: str, tags: Lis
     rednoteArticle = RednoteArticle(title, content, tags, image_urls)
     
     async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(headless=True)
+        browser = await playwright.chromium.launch(headless=False)
         context = await browser.new_context(storage_state="src/rednote_mcp_plus/cookie/rednote_cookies.json")
         page = await context.new_page()
         await page.goto("https://www.xiaohongshu.com/explore")
